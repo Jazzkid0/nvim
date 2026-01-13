d783e8a2b057864572cc89d86ff7f162e80c54ad
Jazzkid0 <jazzkid@jazzkid.xyz>
2026-01-13 22:26:02 +0000

    vim.diagnostic.jump

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,56 +1,66 @@
 -- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
+--  See `:help map()`
+
+local map = vim.keymap.set
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+map('n', '<Esc>', vim.cmd.nohlsearch)
 
-vim.keymap.set('n', '<leader>ff', vim.cmd.Ex, { desc = 'Open netrw file explorer' })
+map('n', '<leader>ff', vim.cmd.Ex, { desc = 'Open netrw file explorer' })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>dl', vim.diagnostic.open_float, { desc = 'Show [D]iagnostics [L]ist' })
-vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>ql', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix [L]ist' })
-vim.keymap.set('n', '[q', '<cmd>cprev<CR>zz', { desc = 'Go to previous [Q]uickfix item' })
-vim.keymap.set('n', '<leader>qp', '<cmd>cprev<CR>zz', { desc = 'Go to previous [Q]uickfix item' })
-vim.keymap.set('n', ']q', '<cmd>cnext<CR>zz', { desc = 'Go to next [Q]uickfix item' })
-vim.keymap.set('n', '<leader>qn', '<cmd>cnext<CR>zz', { desc = 'Go to next [Q]uickfix item' })
+map('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+map('n', '<leader>dl', vim.diagnostic.open_float, { desc = 'Show [D]iagnostics [L]ist' })
+map('n', '<leader>ql', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix [L]ist' })
+-- Diag jumps
+local function diag_jump(user_opts)
+  return function()
+    vim.diagnostic.jump(vim.tbl_extend("force", {
+      reveal = true,
+      on_jump = function()
+        vim.cmd("normal! zz")
+      end,
+    }, user_opts or {}))
+  end
+end
 
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+map('n', '[d' , diag_jump({ count = -1 }), { desc = 'Previous Diagnostic'} )
+map('n', ']d' , diag_jump({ count = 1 }), { desc = 'Next Diagnostic'} )
+map('n', '[e' , diag_jump({ count = -1, severity = vim.diagnostic.severity.ERROR, }), { desc = 'Previous Error'} )
+map('n', ']e' , diag_jump({ count = 1, severity = vim.diagnostic.severity.ERROR, }), { desc = 'Next Error'} )
+map('n', '[w' , diag_jump({ count = -1, severity = vim.diagnostic.severity.WARN, }), { desc = 'Previous Warning'} )
+map('n', ']w' , diag_jump({ count = 1, severity = vim.diagnostic.severity.WARN, }), { desc = 'Next Warning'} )
+
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move focus to the upper window' })
-vim.keymap.set('n', '<C-Left>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-Right>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-Down>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-Up>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+map('n', '<C-h>', '<C-w>h', { desc = 'Move focus to the left window' })
+map('n', '<C-l>', '<C-w>l', { desc = 'Move focus to the right window' })
+map('n', '<C-j>', '<C-w>j', { desc = 'Move focus to the lower window' })
+map('n', '<C-k>', '<C-w>k', { desc = 'Move focus to the upper window' })
+map('n', '<C-Left>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+map('n', '<C-Right>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+map('n', '<C-Down>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+map('n', '<C-Up>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Navigate wrapped text with alt
-vim.keymap.set('n', '<M-j>', 'gj')
-vim.keymap.set('n', '<M-k>', 'gk')
+map('n', '<M-j>', 'gj')
+map('n', '<M-k>', 'gk')
 
 -- Move multiple highlighted lines with capitals
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+map('v', 'J', ":m '>+1<CR>gv=gv")
+map('v', 'K', ":m '<-2<CR>gv=gv")
 
 -- Keep the cursor in the right place (joining, scrolling, searching)
-vim.keymap.set('n', 'J', 'mzJ`z')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
-vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
-vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+map('n', 'J', 'mzJ`z')
+map('n', '<C-d>', '<C-d>zz')
+map('n', '<C-u>', '<C-u>zz')
+map('n', 'n', 'nzzzv')
+map('n', 'N', 'Nzzzv')
 
 -- Find and replace with word under cursor
-vim.keymap.set('n', '<leader>s', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', { desc = 'Find and replace word' })
+map('n', '<leader>s', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', { desc = 'Find and replace word' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
