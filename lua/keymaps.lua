4d07eda0e21264f4f8f9740723db6c02a7ee6a86
Jazzkid0 <jazzkid@jazzkid.xyz>
2026-01-15 17:03:39 +0000

    loc/qflist buffer autoname

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -73,4 +73,18 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Name the loclist/quickfix buffer
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "qf",
+  callback = function()
+    local win = vim.api.nvim_get_current_win()
+    local info = vim.fn.getwininfo(win)[1]
+    local buf = vim.api.nvim_get_current_buf()
+    if info and info.loclist == 1 then
+      vim.api.nvim_buf_set_name(buf, "loclist")
+    else
+      vim.api.nvim_buf_set_name(buf, "quickfix")
+    end
+  end,
+})
 -- vim: ts=2 sts=2 sw=2 et
