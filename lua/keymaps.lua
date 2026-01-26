de9cd062304734db044290c348f67a287048d20a
Jazzkid0 <jazzkid@jazzkid.xyz>
2026-01-26 23:18:59 +0000

    <leader>o :only

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -42,6 +42,7 @@ map('n', '<C-Left>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 map('n', '<C-Right>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 map('n', '<C-Down>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 map('n', '<C-Up>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+map('n', '<leader>o', '<C-w><C-o>', { desc = '[O]nly show this window' })
 
 -- Navigate wrapped text with alt
 map('n', '<M-j>', 'gj')
